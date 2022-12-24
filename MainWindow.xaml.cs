296424cfe1e82a3978a70542240398db00c099c4
Алексей <lebedevalexeyo@gmail.com>
2022-12-25 03:49:26 +1100

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName1 = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontSize = FontSize;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Italic)
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+            else textBox.FontStyle = FontStyles.Italic;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations != TextDecorations.Underline)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+               
+            }
+            else textBox.TextDecorations = null;
+        }
+        //private void Button_Click_3(object sender, RoutedEventArgs e)
+        //{
+        //    if (textBox.TextDecorations != TextDecorations.Strikethrough)
+        //    {
+        //        textBox.TextDecorations = TextDecorations.Strikethrough;
+
+        //    }
+        //    else textBox.TextDecorations = null;
+        //}
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Black;
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Red;
+        }
+
+   
+    }
+}
